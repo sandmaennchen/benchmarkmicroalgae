293c773097c0cfaa111aa9a99bd7d33dca54cce5
guzmanjl <joguzman@ual.es>
2025-12-17 11:47:05 +0100

    Create Benchmark_main.m

---
+++ player/Benchmark_main.m
@@ -0,0 +1,20 @@
+%% ===== Benchmark Microalgae Raceway Reactor =====
+clear; clc; rng(42,'twister');
+addpath('..');
+
+%% ---------------- Data load ----------------
+load_data;
+
+%% ---------------- Controllers (one per actuator) ----------------
+ctrl = struct();
+ctrl.fn_pH_CO2  = @controller_pH_OnOff;
+ctrl.fn_DO_air  = @controller_DO_OnOff;
+ctrl.fn_HD      = @controller_HD_fixed;
+ctrl.fn_Temp_HX = @controller_Temp_HX_no_control;
+
+%% ---------------- Simulation ----------------
+fprintf('\nRunning Benchmark Microalgae Raceway simulation...\n');
+[results] = simulate_benchmark_model(Data, ctrl);
+
+%% ---------------- Results ----------------
+show_results;
